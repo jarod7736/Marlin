5d926df033a595deac1004dac1f8c636e9bdc046
Aurora4891 <60441042+Aurora4891@users.noreply.github.com>
2020-01-30 06:26:08 -0600

    Add files via upload

---
+++ Marlin/src/pins/stm32/pins_MKS_ROBIN_NANO_ELF.h
@@ -0,0 +1,34 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (c) 2019 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#pragma once
+
+/**
+ * MKS Robin nano (STM32F130VET6) board pin assignments
+ */
+#include "pins_MKS_ROBIN_NANO.h"
+
+#undef PS_ON_PIN
+#define PS_ON_PIN          PB2   // PA3 PW_OFF
+
+#undef LED_PIN       //  PS_ON_PIN
+#undef LCD_RESET_PIN // FSMC_RST
+#define MKS_ROBIN_TFT35     //Uncomment when using the robin tft35 display
