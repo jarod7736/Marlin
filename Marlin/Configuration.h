bcfed227485d2d2d6578853cd1decff5679ad1d5
Jarod Belshaw <jbelshaw@amd.com>
2020-02-11 08:33:45 -0600

    updated extrusion steps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -753,7 +753,7 @@
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
 //@ settings from 1.0.3 stock firmware
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 381.5 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 406.09 }
 
 /**
  * Default Max Feed Rate (mm/s)
